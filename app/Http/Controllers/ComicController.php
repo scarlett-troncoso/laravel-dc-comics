53d78476bccd973574575be3f179515dd7b5bc65
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-06 17:27:32 +0200

    add dd in functions index and show for view of table database in page

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -12,7 +12,8 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        // 
+       dd(Comic::all());
     }
 
     /**
@@ -37,6 +38,7 @@ class ComicController extends Controller
     public function show(Comic $comic)
     {
         //
+        dd($comic);
     }
 
     /**
