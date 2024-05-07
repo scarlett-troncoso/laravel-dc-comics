dae4f3d66eaec45308af600da91c201dac41d134
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-07 17:37:09 +0200

    add comics in home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h3 class="text-center">
-            <a href=" {{ route('comics.index') }} "> Vedere i Comics</a>
-        </h3>
+    <div class="container d-flex">
+        <!-- @*dd($comics) -->
+        @foreach ($comics as $comic)
+            <div class="card m-3" style="width: 18rem;">
+                <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $comic->title }}</h5>
+                    <p class="card-text">{{ $comic->series }}</p>
+                    <a href=" {{ route('comics.show', $comic) }} " class="btn btn-primary">View</a>
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
