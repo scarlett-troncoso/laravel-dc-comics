20d7a50b4481fac99d3194a0f05d02e75536a998
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-07 17:36:01 +0200

    create table of all comics

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -1,17 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container d-flex">
-        <!-- @*dd($comics) -->
-        @foreach ($comics as $comic)
-            <div class="card m-3" style="width: 18rem;">
-                <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $comic->title }}</h5>
-                    <p class="card-text">{{ $comic->series }}</p>
-                    <a href=" {{ route('comics.show', $comic) }} " class="btn btn-primary">View</a>
-                </div>
-            </div>
-        @endforeach
+    <div class="container">
+        <div class="table-responsive container">
+            <a class="btn btn-primary rounded-pill position-fixed bottom-0 end-0 m-3" href=" {{ route('comics.create') }} "
+                role="button">
+                <i class="fa-solid fa-plus"></i>
+                <span>ADD</span>
+            </a>
+            <table class="table table-primary fs-6">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Thumb</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Series</th>
+                        <th scope="col">Sale date</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Created at</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($comics as $comic)
+                        <tr class="">
+                            <td scope="row">{{ $comic->id }}</td>
+                            <td>{{ $comic->title }}</td>
+                            <td>
+                                <img width="100" src="{{ $comic->thumb }}" alt="">
+                            </td>
+                            <td>{{ $comic->price }}</td>
+                            <td>{{ $comic->series }}</td>
+                            <td>{{ $comic->sale_date }}</td>
+                            <td>{{ $comic->type }}</td>
+                            <td>{{ $comic->created_at }}</td>
+                            <td> <a href="{{ route('comics.show', $comic) }}">View</a> </td>
+                        </tr>
+                    @empty
+                        <tr class="">
+                            <td scope="row">no comics yet</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
