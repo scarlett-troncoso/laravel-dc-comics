627d4352d3f5fed2da5efd6607dbf201abd2ba55
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-11 17:00:46 +0200

    add the error directive in inputs of form and value for repopulating

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -2,42 +2,66 @@
 
 @section('content')
     <div class="container">
+        @include('partials.validation-errors')
         <form action="{{ route('comics.store') }}" method="POST">
             <!--ATTENZIONE: la route é 'comics.store' e NON 'admin.comics.store' perche: é una Route fa riferimento alla Route 'comics' e non alla cartella comics di views-->
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" name="title" id="title" aria-describedby="titleHelper"
-                    placeholder="Type a title of comic" />
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
+                    aria-describedby="titleHelper" placeholder="Type a title of comic" value="{{ old('title') }}" />
+                @error('title')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
+                    rows="3"> {{ old('description') }} </textarea>
+                @error('description')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Insert link image</label>
-                <input type="text" class="form-control" name="thumb" id="thumb" aria-describedby="thumbHelper"
-                    placeholder="https//...." />
+                <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb"
+                    id="thumb" aria-describedby="thumbHelper" placeholder="https//...." value="{{ old('thumb') }}" />
+                @error('thumb')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
-                <input type="text" class="form-control" name="price" id="price" aria-describedby="priceHelper"
-                    placeholder="50.89" />
+                <input type="text" class="form-control @error('price') is-invalid @enderror" name="price"
+                    id="price" aria-describedby="priceHelper" placeholder="50.89" value="{{ old('price') }}" />
+                @error('price')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
-                <input type="text" class="form-control" name="series" id="series" aria-describedby="seriesHelper"
-                    placeholder="Series ..." />
+                <input type="text" class="form-control @error('series') is-invalid @enderror" name="series"
+                    id="series" aria-describedby="seriesHelper" placeholder="Series ..." value="{{ old('series') }}" />
+                @error('series')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Sale date</label>
-                <input type="date" class="form-control" name="sale_date" id="sale_date"
-                    aria-describedby="sale_dateHelper" placeholder="2022-04-25" />
+                <input type="date" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date"
+                    id="sale_date" aria-describedby="sale_dateHelper" placeholder="2022-04-25"
+                    value="{{ old('sale_date') }}" />
+                @error('sale_date')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
-                <input type="text" class="form-control" name="type" id="type" aria-describedby="typeHelper"
-                    placeholder="Type...." />
+                <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type"
+                    aria-describedby="typeHelper" placeholder="Type...." value="{{ old('type') }}" />
+                @error('type')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Save</button>
         </form>
