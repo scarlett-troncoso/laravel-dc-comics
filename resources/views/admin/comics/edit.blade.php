da8c11b49b578569e5a13054fb6f901e2537fc0d
scarlett-troncoso <scar.labussieri@gmail.com>
2024-05-11 17:20:43 +0200

    add diplaying the validation error and repopulating forms in form of edit

--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -5,6 +5,9 @@
 
     <h2>Edit comic {{ $comic->id . ':  ' . $comic->title }}</h2>
     <div class="container">
+
+        @include('partials.validation-errors')
+
         <form action="{{ route('comics.update', $comic) }}" method="POST">
             @csrf
             @method('PUT')
@@ -12,36 +15,58 @@
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control" name="title" id="title" aria-describedby="titleHelper"
-                    placeholder="Type a title of comic" value="{{ $comic->title }}" />
+                    placeholder="Type a title of comic" value="{{ old('title', $comic->title) }}" />
+                @error('title')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                <textarea class="form-control" name="description" id="description" rows="3">{{ $comic->description }}</textarea>
+                <textarea class="form-control" name="description" id="description" rows="3">{{ old('description', $comic->description) }}</textarea>
+                @error('description')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Insert link new image</label>
                 <input type="text" class="form-control" name="thumb" id="thumb" aria-describedby="thumbHelper"
-                    placeholder="https//...." value="{{ $comic->thumb }}" />
+                    placeholder="https//...." value="{{ old('thumb', $comic->thumb) }}" />
+                @error('thumb')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
                 <input type="text" class="form-control" name="price" id="price" aria-describedby="priceHelper"
-                    placeholder="50.89" value="{{ $comic->price }}" />
+                    placeholder="50.89" value="{{ old('price', $comic->price) }}" />
+                @error('price')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
                 <input type="text" class="form-control" name="series" id="series" aria-describedby="seriesHelper"
-                    placeholder="Series ..." value="{{ $comic->series }}" />
+                    placeholder="Series ..." value="{{ old('series', $comic->series) }}" />
+                @error('series')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Sale date</label>
                 <input type="date" class="form-control" name="sale_date" id="sale_date"
-                    aria-describedby="sale_dateHelper" placeholder="2022-04-25" value="{{ $comic->sale_date }}" />
+                    aria-describedby="sale_dateHelper" placeholder="2022-04-25"
+                    value="{{ old('sale_date', $comic->sale_date) }}" />
+                @error('sale_date')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
                 <input type="text" class="form-control" name="type" id="type" aria-describedby="typeHelper"
-                    placeholder="Type...." value="{{ $comic->type }}" />
+                    placeholder="Type...." value="{{ old('type', $comic->type) }}" />
+                @error('type')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Save Edition</button>
         </form>
